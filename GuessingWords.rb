2284c96c1283efedbff4588afa503a3a540144b8
jacobknighj12 <78334099+jacobknighj12@users.noreply.github.com>
2021-04-19 16:16:18 +1000

    txt file import and use/cleanup/polish/etc

--- GuessingWords.rb
+++ GuessingWords.rb
@@ -1,74 +1,55 @@
-question1 = " A viscous subsance"
-answer1 = "honey"
+File.open("questionAndAnswerFile.txt", "r") do |questionAndAnswerFile|
 
-question2 = " The only start to a coders day"
-answer2 = "coffee"
-
-question3 = " The worst online meeting software, please stop them from using it, god please"
-answer3 = "zoom"
-
-question4 = " The best game laucher/marketplace"
-answer4 = "steam"
-
-question = question1
-answer = answer1
-
-playerInput = ""
 index = 0
+questionArray = questionAndAnswerFile.readlines()
+questionAndAnswer = questionArray[index].split("|")
+question = questionAndAnswer[0].strip()
+answer = questionAndAnswer[1].strip()
+playerInput = ""
 lives = 3
 score = 0
-#if score = 3 gameover
-#Hangman game
-#start with getting random word// fuck that shit lets just put in fixed qna for now
-#after getting random word and putting it into var get 2 -3 words of definition of word
-#ask player to guess
-#3 stikes then reset
 
-puts "Welcome!"     #    "%s "    % answer
-while index <= 4
-puts "Here is your hint: %s" % question
+puts "Welcome!"
+while lives >=1 and repeat == true
+  puts "Here is your hint: %s" % question
+  playerInput = gets
 
-playerInput = gets
+  if playerInput.include? answer
+    score += 1
+    index +=1
+    print "Correct! %s" % answer + " Your score: %s" % score
+    puts " lives left: %s" % lives
+    if index < questionArray.length()
+      questionAndAnswer = questionArray[index].split("|")
+      question = questionAndAnswer[0].strip()
+      answer = questionAndAnswer[1].strip()
+      else
+        puts "End of game! Thanks for playing!"
+        puts "Play again?"
+        puts "y/n"
+        playerInput = gets
+          if playerInput.include? "n" then break
+            end
+          if playerInput.include? "y" then index = 0
+            questionAndAnswer = questionArray[index].split("|")
+            question = questionAndAnswer[0].strip()
+            answer = questionAndAnswer[1].strip()
+          end
+      end
+    else
+      puts "failure"
+      lives -= 1
+      puts "lives left: %d" % lives
+    if lives >= 1 then puts "Try again!"
+      puts "the word starts with a %s" % answer[0]
 
-if playerInput.include? answer # = true then +1 "H" "o" "n" "e" "y"
-  score += 1
-  index +=1
-  print "Correct! " + answer + ", Your score: " + score.to_s
-  puts " lives left: " + lives.to_s
-
-  if score == 4 then index = 6
-  end
-  if index == 1
-    answer = answer2
-    question = question2
-  end
-  if index == 2
-    answer = answer3
-    question = question3
-  end
-  if index == 3
-    answer = answer4
-    question = question4
-  end
-#  puts "Next question!:" + question
-else
-  puts "failure"
-  lives -= 1
-  print "lives left: "
-  puts lives
-  if lives >= 1 then puts "Try again!"
-  puts "the word starts with a " + answer[0]
-
+      end
+    end
   end
 end
- if lives == 0 then index = 6
- end
-end
 
-puts "End of game! Thanks for playing!"
-while index == 5
-puts "Play again?"
 
-end
+
+
 #standard library (learn this specificly)(rubycore)(import shit or random numbers)
 # %d for in string stuff
